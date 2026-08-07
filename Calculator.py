b6d41c3f3a1d849e376a4146c68095493e58a8c3
harshitsonitech-eng <harshitsoni.tech@gmail.com>
2026-08-07 20:32:00 +0530

    Initial commit

---
+++ Calculator.py
@@ -0,0 +1,21 @@
+#Simple calculator
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+addition = num1 + num2
+substraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+floor_division = num1 // num2
+modulus = num1 % num2
+exponentitions = num1 ** num2
+
+print(f"Addition : {addition}")
+print(f"Substraction : {substraction}")
+print(f"Multiplication : {multiplication}")
+print(f"Division : {division}")
+print(f"Floor Dividion : {floor_division}")
+print(f"Modulus : {modulus}")
+print(f"Exponentitions : {exponentitions}")
+
